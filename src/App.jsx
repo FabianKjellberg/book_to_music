1ac2c2466d7a81724a36425b986383022dc3d7d5
alexandra <83598143+gorillagripcore@users.noreply.github.com>
2023-12-08 12:50:43 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import  StartPage from './startpage'
+import  MixBook from './mixbook'
+
+import './index.css'
+import bannerImage from './img/banner.png'
 
 function App() {
   return (
     <>
-     <StartPage></StartPage>
+      <img className="banner" src={bannerImage} alt="Banner" />
+      <StartPage></StartPage>
+      {/*<MixBook></MixBook> */}
+      <div class="footer">
+          <p>★ BookMixtape</p>
+      </div>
     </>
   )
 }
