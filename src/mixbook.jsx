2843b9721b89df71dd810b57c35c092a804f0937
alexandra <83598143+gorillagripcore@users.noreply.github.com>
2023-12-08 13:38:48 +0100

    🧸

--- src/mixbook.jsx
+++ src/mixbook.jsx
@@ -1,10 +1,52 @@
+<<<<<<< HEAD
+import './mixbook.css'
 
 function MixBook() {
+  const fixedGradient = "linear-gradient(90deg, rgba(66,109,170,1) 10%, rgba(175,185,201,1) 68%)";
   return (
     <div>
       <h1>Type</h1>
+
+      <div className="flex-container">
+        <img
+          className="star-divider"
+          src="https://cdn.discordapp.com/attachments/1078963035630223391/1135191668145012796/asedfsed-removebg.png"
+          alt="Star Divider"
+        />
+      </div>
+
+      <h1>Genre</h1>
+
+      <div className="flex-container">
+        <img
+          className="star-divider"
+          src="https://cdn.discordapp.com/attachments/1078963035630223391/1135191668145012796/asedfsed-removebg.png"
+          alt="Star Divider"
+        />
+      </div>
+
+      <h1>File</h1>
+
+      <div className="flex-container">
+        <button
+          className="start-button row"
+          style={{ background: fixedGradient }}
+        >
+          <p>Confirm</p>
+          <img
+            src="https://cdn.discordapp.com/attachments/1078963035630223391/1136323255955898388/616818.png"
+            alt="Icon"
+          />
+        </button>
+      </div>
+=======
+function mixbook() {
+  return (
+    <div>
+      <h1>Hello</h1>
+>>>>>>> df5b8b6767fc12bbce9efce037fa13867417f06d
     </div>
   );
 }
 
-export default MixBook;
+export default mixbook;
