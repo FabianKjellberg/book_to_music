28989f6077b3c584aeecb71c5fd49d7613a15e96
FabianKjellberg <Fabian.Kjellberg98@gmail.com>
2023-12-11 14:57:15 +0100

    updated key

--- src/openAiUtility.jsx
+++ src/openAiUtility.jsx
@@ -2,14 +2,17 @@ import OpenAi from 'openai'
 
 class OpenAiUtility {
 
+
     static async getSentiment(text) {
+        const key = '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';
+
         try {
-            const openai = new OpenAi({apiKey: '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', dangerouslyAllowBrowser: true})
+            const openai = new OpenAi({apiKey: key, dangerouslyAllowBrowser: true})
 
             const completion = await openai.chat.completions.create({
-                
 
-                messages: [{ role: "system", content: "what is 10 + 10?" }],
+
+                messages: [{ role: "system", content: "anwer in format [emotion1(ex. happy, sad), probability1(0-1)] repeated. Analyse this text:" + text}],
                 model: "gpt-3.5-turbo",
             });
 
