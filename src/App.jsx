258060fed61c1452394778651562ddcd91c762c9
Valentino Glave <defliez@protonmail.com>
2023-12-15 22:22:46 +0100

    Updated App.jsx with routes /spotify-auth and /spotify-playlists.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import StartPage from './startpage';
 import MixBook from './mixbook';
 import GoogleTest from './googleTest';
+import SpotifyAuthorization from "./SpotifyAuthorization";
+import SpotifyPlaylists from "./SpotifyPlaylists";
 
 function App() {
     return (
@@ -10,6 +12,8 @@ function App() {
                 <Route path="/" element={<StartPage />} />
                 <Route path="/mixbook" element={<MixBook />} />
                 <Route path="/googletest" element={<GoogleTest />} />
+                <Route path="/spotify-auth" element={<SpotifyAuthorization />} />
+                <Route path="/spotify-playlists" element={<SpotifyPlaylists />} />
             </Routes>
         </>
     );
