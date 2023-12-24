0adac5ced346df817999f71481a3005ea86c817e
Valentino Glave <defliez@protonmail.com>
2023-12-24 02:46:30 +0100

    feat(SpotifyAddPlaylist): add new file for Spotify playlist creation functionality
    This commit adds a new file, `SpotifyAddPlaylist.jsx`, which implements a feature for adding playlists on Spotify. It includes several state variables to handle playlist attributes such as name, description, and public visibility. It also provides error handling for playlist creation.

---
+++ src/SpotifyAddPlaylist.jsx
@@ -0,0 +1,106 @@
+/*
+ * Är för tillfället sin egen endpoint /add-playlist
+ *
+ */
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const SpotifyAddPlaylist = () => {
+    const [accessToken, setAccessToken] = useState('');
+    // state för att skapa en playlist
+    const [playlistName, setPlaylistName] = useState('');
+    const [playlistDescription, setPlaylistDescription] = useState('');
+    const [playlistPublic, setPlaylistPublic] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        // Extract token from URL hash
+        const token = new URLSearchParams(window.location.hash.substring(1)).get('access_token');
+
+        // Set the access token in state
+        if (token) {
+            setAccessToken(token);
+        }
+    }, []);
+
+    const handlePlaylistNameChange = (event) => {
+        setPlaylistName(event.target.value);
+    };
+
+    const handlePlaylistDescriptionChange = (event) => {
+        setPlaylistDescription(event.target.value);
+    };
+
+    const handlePlaylistPublicChange = (event) => {
+        setPlaylistPublic(event.target.checked);
+    };
+
+    const handleCreatePlaylist = async () => {
+        try {
+            const response = await axios.post(
+                'https://api.spotify.com/v1/me/playlists',
+                {
+                    name: playlistName,
+                    description: playlistDescription,
+                    public: playlistPublic,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                }
+            );
+
+            console.log(response.data);
+        } catch (err) {
+            setError(err.message || 'Error creating playlist');
+        }
+    };
+
+    return (
+        <div>
+            {accessToken ? (
+                <>
+                    <h2>Create a Spotify Playlist</h2>
+                    <div>
+                        <label htmlFor="playlist-name">Name</label>
+                        <input
+                            id="playlist-name"
+                            type="text"
+                            value={playlistName}
+                            onChange={handlePlaylistNameChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="playlist-description">Description</label>
+                        <input
+                            id="playlist-description"
+                            type="text"
+                            value={playlistDescription}
+                            onChange={handlePlaylistDescriptionChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="playlist-public">Public</label>
+                        <input
+                            id="playlist-public"
+                            type="checkbox"
+                            checked={playlistPublic}
+                            onChange={handlePlaylistPublicChange}
+                        />
+                    </div>
+                    <button onClick={handleCreatePlaylist}>Create Playlist</button>
+                </>
+            ) : (
+                <p>No access token available. Make sure you completed the Spotify authorization process.</p>
+            )}
+
+            {error && <p>Error: {error}</p>}
+        </div>
+    );
+
+
+
+};
+
+export default SpotifyAddPlaylist;
