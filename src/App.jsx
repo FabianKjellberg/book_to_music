d5bcc18c7372a67e8f01c6ff39e55f69c1040b2a
Junmiann <Mimmixu@gmail.com>
2023-12-28 14:10:51 +0100

    satte banner tillbaka

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import MixBook from './mixbook';
 import SpotifyAuthorization from "./SpotifyAuthorization";
 import SpotifyPlaylists from "./SpotifyPlaylists";
 import SpotifyAddPlaylist from "./SpotifyAddPlaylist";
+import bannerImage from './img/banner.png'
 
 
 
 function App() {
     return (
         <>
+        <img className="banner" src={bannerImage} alt="Banner" />
             <Routes className="App">
                 <Route path="/" element={<StartPage />} />
                 <Route path="/mixbook" element={<MixBook />} />
