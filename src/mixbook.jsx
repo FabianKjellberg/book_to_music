4f427aeb9cb1e40a54ad71bae7b55d2a2593db4c
Junmiann <Mimmixu@gmail.com>
2024-01-05 11:11:15 +0100

    tagit bort genre och typ

--- src/mixbook.jsx
+++ src/mixbook.jsx
@@ -21,56 +21,21 @@ function MixBook() {
         }
     });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
   return (
     <div>
         { accessToken ? (
             <>
-      <h1>Type</h1>
       <div className="flex-container">
-        <form onSubmit={handleSubmit}>
-          <div className="button">
-            <input type="radio" name="type" value="page" id="page" />
-            <label htmlFor="page">Page</label>
-
-            <input type="radio" name="type" value="chapter" id="chapter" />
-            <label htmlFor="chapter">Chapter</label>
-
-            <input type="radio" name="type" value="book" id="book" />
-            <label htmlFor="book">Book</label>
-          </div>
-        </form>
+      <Link to={`/add-playlist#access_token=${accessToken}`} className="button-link">
+        <button className="start-button row generate_playlist_button">
+          <p>Create new playlist</p>
+        </button>
+      </Link>
       </div>
 
       <div className="flex-container">
         <img
-          className="star-divider"
-          src="https://cdn.discordapp.com/attachments/1078963035630223391/1184154715232161953/star.png?ex=658af104&is=65787c04&hm=0c67a1b2fda1045a48eda37875929485ff989e763e7b31289de05382d8c5a98b&"
-          alt="Star Divider"
-        />
-      </div>
-
-      <h1>Genre</h1>
-      <div className="flex-container">
-        <form onSubmit={handleSubmit}>
-          <div className="button">
-            <input type="radio" name="type" value="lofi" id="lofi" />
-            <label htmlFor="lofi">Lo-Fi</label>
-
-            <input type="radio" name="type" value="classical" id="classical" />
-            <label htmlFor="classical">Classical</label>
-
-            <input type="radio" name="type" value="pop" id="pop" />
-            <label htmlFor="pop">Pop</label>
-          </div>
-        </form>
-      </div>
-
-      <div className="flex-container">
-        <img
-          className="star-divider"
+          className="star-divider mixbook-star-divider"
           src="https://cdn.discordapp.com/attachments/1078963035630223391/1184154715232161953/star.png?ex=658af104&is=65787c04&hm=0c67a1b2fda1045a48eda37875929485ff989e763e7b31289de05382d8c5a98b&"
           alt="Star Divider"
         />
@@ -79,14 +44,6 @@ function MixBook() {
       <h1>File</h1>
       <div>
         <EpubViewer></EpubViewer>
-      </div>
-
-      <div className="flex-container">
-        <Link to={`/add-playlist#access_token=${accessToken}`} className="button-link">
-          <button className="start-button row generate_playlist_button">
-            <p>Generate playlist</p>
-          </button>
-        </Link>
       </div>
 
             </>
